191c28f13f4d364627f76f64188378af4ba31258
= <=>
2026-06-06 15:56:01 +0530

    day 1 Python Foundations for AI

---
+++ python/day1/datatypes.py
@@ -0,0 +1,10 @@
+name = "Siva"      # String
+age = 22           # Integer
+height = 5.8       # Float
+is_student = True  # Boolean
+
+
+print(name)
+print(age)
+print(height)
+print(is_student)
---
+++ python/day1/variables.py
@@ -0,0 +1,7 @@
+name = "Siva"
+age = 22
+height = 5.8
+
+print(name)
+print(age)
+print(height)
