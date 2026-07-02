885246e5ae1db0395891bf09664bea47b46f58fa
Solapuram Siva Kumar Reddy <solapuramsivakumar703@gmail.com>
2026-07-02 06:40:16 +0530

    fixed the class issue

--- python/day13/employee.py
+++ python/day13/employee.py
@@ -3,10 +3,12 @@ class Employee:
         self.employee_id = employee_id
         self.name = name
         self.salary = salary
-        return self.employee_id,self.name,self.salary
 
 emp1 = Employee()
 emp2 = Employee()
 
-print(emp1.employee_details(1,"Kiran",25000))
-print(emp2.employee_details(2,"sunil",35000))
+
+emp1.employee_details(1,"Kiran",25000)
+emp2.employee_details(2,"sunil",35000)
+print(emp1.employee_id,emp1.name,emp1.salary)
+print(emp2.employee_id,emp2.name,emp2.salary)
